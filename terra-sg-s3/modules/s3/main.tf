582dcbea0459ea66b5f86e0b1e28d4cf0a2f213c
asatishpaul <101627595+asatishpaul@users.noreply.github.com>
2025-01-02 19:19:23 +0530

    Create main.tf

---
+++ terra-sg-s3/modules/s3/main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tags = {
+    Environment = "Terraform"
+    Module      = "S3"
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.enable_versioning ? "Enabled" : "Suspended"
+  }
+}
